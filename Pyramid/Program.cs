bec239fb2674947f35881332ffaad5e67535d1bc
Your Name <you@email.com>
2016-04-14 16:46:17 -0400

    Initial commit. Pyramid exercise.

---
+++ Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //outer rows - total will be 5
+            //columns total will be 9
+            Console.WriteLine();
+            int rows = 0;
+            int spaces = 0;
+            int stars = 1;
+            for (rows = 0; rows <= 5; rows++)
+            {
+                for (spaces = 5; spaces > rows; spaces--)
+                {
+                    Console.Write(" ");
+                }
+                for (stars = 1; stars < (spaces + rows); stars++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
